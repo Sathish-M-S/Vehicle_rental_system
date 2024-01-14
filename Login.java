8c7d01f2b926d67bd46b26b2838b02d8b826d0cb
Sathish-M-S <155664711+Sathish-M-S@users.noreply.github.com>
2024-01-14 19:54:22 +0530

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,14 +1,11 @@
 package VehicleRentalSystem;
 import java.util.Scanner;
-
 public class Login 
 {
 	Admin    ad=new Admin();
 	Customer cs=new Customer(); 
 	static Scanner scan=new Scanner(System.in);
 	static int pass=0;
-	//-----------------------------------------
-	
 	public int user()
 	{
 		while(pass==0)
@@ -18,15 +15,12 @@ public class Login
 		//------
 		return 1;
 	}
-	//-----------------------------------------
-	
 	public int admin()
 	{
 		while(pass==0)
 		{
 			pass=ad.checkKey();
 		}
-		//------
 		return 1;
 	}
 }
